27f5eaec94d175926f4861b693eee2c7a05fe138
jiho <ghrms80@gmail.com>
2023-01-13 16:41:20 +0900

    Creating a FutureBuilder

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,18 +1,37 @@
+import 'package:ch05_scrollable_widgets/models/explore_data.dart';
 import 'package:flutter/material.dart';
 
 import '../api/mock_fooderlich_service.dart';
 
 class ExploreScreen extends StatelessWidget {
-  // 1
   final mockService = MockFooderlichService();
   ExploreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // 2
-    // TODO: Add TodayRecipeListView FutureBuilder
-    return const Center(
-      child: Text('Explore Screen'),
+    // 1
+    return FutureBuilder(
+      // 2
+      future: mockService.getExploreData(),
+      // 3
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // 4
+        if (snapshot.connectionState == ConnectionState.done) {
+          // 5
+          final recipes = snapshot.data?.todayRecipes ?? [];
+          // TODO: Replace this with TodayRecipeListView
+          return Center(
+            child: Container(
+              child: const Text('Show TodayRecipeListView'),
+            ),
+          );
+        } else {
+          // 6
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
